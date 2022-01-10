477c45687b12add38a6880d3c618c218058791ae
umihico <umihico@users.noreply.github.com>
2022-01-10 18:25:21 +0900

    hello world

---
+++ config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeebCxQopqlY-1XPvmKK-_FNjL3zqlpgI",
+  authDomain: "social-auth-hello-world.firebaseapp.com",
+  projectId: "social-auth-hello-world",
+  storageBucket: "social-auth-hello-world.appspot.com",
+  messagingSenderId: "534391144830",
+  appId: "1:534391144830:web:a148453ae048428a63f32f",
+};
